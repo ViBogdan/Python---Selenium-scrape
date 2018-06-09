2ec8807a43b375efe308b01459e586b4f52a0f94
Gabriel Gradinaru <gabriel.gradinaru.gt@gmail.com>
2018-06-09 20:39:07 +0300

    Extract items from dualListBox

--- manifest_cultural/static/js/main.js
+++ manifest_cultural/static/js/main.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    let dualListBox = new DualListbox('.demo');
+var dualListBox;
+
+$(document).ready(function(event) {
+    dualListBox = new DualListbox('.demo');
 });
+
+function generateEventList() {
+    // plain js
+    document.getElementById('genlist').innerHTML = dualListBox.selectedList.innerText
+
+    // or the same thing using jquery:
+    //$('#genlist').html(dualListBox.selectedList.innerText);
+}
+
